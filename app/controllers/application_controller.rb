f8b4e72ee93c2a17db1740315c25ede973b513f0
Morshed Alam <morshed201@gmail.com>
2012-12-24 13:28:47 +0600

    Removed unnecessary files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  layout :set_layout
-
-  private
-
-  def set_layout
-    return request.headers['X-PJAX'] ? false : 'application'
-  end
 end
